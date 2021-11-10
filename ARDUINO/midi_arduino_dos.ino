1f876b8dc6224ed7bd4a890b19eebf6de9f03d42
Alejo Cortiñaz <masalejo8@gmail.com>
2021-11-10 15:51:28 -0300

    Add files via upload

--- ARDUINO/midi_arduino_dos.ino
+++ ARDUINO/midi_arduino_dos.ino
@@ -1,4 +1,3 @@
-int thr;
 
 //variables que leen los valores analogicos de los paneles
 int panel_1;
@@ -24,24 +23,28 @@ int var_8;
 int var_9;
 int var_10;
 
+//variable que controla el delay entre la lectura de los paneles
+int delayentrepaneles = 25;
+//variable que controla el threshold para detectar el laser y mandar la señal
+int thr = 250;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-panel_1  = 0;
-panel_2  = 0;
-panel_3  = 0;
-panel_4  = 0;
-panel_5  = 0;
-panel_6  = 0;
-panel_7  = 0;
-panel_8  = 0;
-panel_9  = 0;
-panel_10 = 0;
+  panel_1  = 0;
+  panel_2  = 0;
+  panel_3  = 0;
+  panel_4  = 0;
+  panel_5  = 0;
+  panel_6  = 0;
+  panel_7  = 0;
+  panel_8  = 0;
+  panel_9  = 0;
+  panel_10 = 0;
 
 
-//  300 = 300;
+  //  300 = 300;
 
 }
 
@@ -49,130 +52,132 @@ void loop() {
 
   //lectura de los sensores
   panel_1 = analogRead(A11);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_2 = analogRead(A10);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_3 = analogRead(A9);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_4 = analogRead(A8);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_5 = analogRead(A7);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_6 = analogRead(A6);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_7 = analogRead(A5);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_8 = analogRead(A4);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_7 = analogRead(A3);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_9 = analogRead(A2);
-  delay(50);
-  
+  delay(delayentrepaneles);
+
   panel_10 = analogRead(A1);
   delay(50);
-  
+
   panel_11 = analogRead(A0);
-  delay(50);
-//-----------------------------------------------
-  /*
-   //Chekeo del panel 1
-   //chanel 1 = 144
-        if (panel_1 > 300 && var_1 == 0) {
+  delay(delayentrepaneles);
+  //-----------------------------------------------
+
+
+    //Chekeo del panel 1
+    //chanel 1 = 144
+        if (panel_1 > thr && var_1 == 0) {
           var_1 = 1;
-         // Serial.println(panel1);
+
           Serial.write(144);
           Serial.write(60);
           Serial.write(100);
-          //Serial.println(panel1);
-          
+         // Serial.println(panel_1);
+
         }
-        else if (panel_1< 300 && var_1 == 1){
+        else if (panel_1 <= thr && var_1 == 1 ){
+
           var_1 = 2;
           Serial.write(144);
           Serial.write(60);
           Serial.write(0);
-          
+
         }
-        else{
+        else if (panel_1 <= thr && var_1 == 2 ){
           var_1=0;
           }
 
-   //Chekeo del panel 2
-   //chanel 2 = 145
-        if (panel_2 > 300 && var_2 == 0) {
+    //Chekeo del panel 2
+    //chanel 2 = 145
+        if (panel_2 >= thr-1 && var_2 == 0) {
           var_2 = 1;
          // Serial.println(panel1);
           Serial.write(145);
           Serial.write(60);
           Serial.write(100);
           //Serial.println(panel1);
-          
+
         }
-        else if (panel_2 <= 300 && var_2 == 1){
+        else if (panel_2 <= thr && var_2 == 1){
           var_2 = 2;
           Serial.write(145);
           Serial.write(60);
           Serial.write(0);
-          
+
         }
-        else{
+        else if (panel_2 <= thr && var_2 == 2){
           var_2=0;
           }
+  
 
-          */
-        //Chekeo del panel 3
-        //chanel 3 = 146
-        if (panel_3 > 300 && var_3 == 0) {
-          var_3 = 1;
-        // Serial.println(panel_3);
-          Serial.write(146);
-          Serial.write(60);
-          Serial.write(100);
-         //Serial.println(panel1);
-          
-        }
-        else if (panel_3 < 300 && var_3 == 1){
-          var_3 = 2;
-          Serial.write(146);
-          Serial.write(60);
-          Serial.write(0);
-          
-        }
-        else{
-          var_3=0;
+  //Chekeo del panel 3
+  //chanel 3 = 146
+  if (panel_3 > thr && var_3 == 0) {
+    var_3 = 1;
+    
+     Serial.write(146);
+     Serial.write(60);
+     Serial.write(100);
+     //Serial.println(panel_3);
+
+  }
+  else if (panel_3 <= thr && var_3 == 1) {
+    var_3 = 2;
+   Serial.write(146);
+   Serial.write(60);
+   Serial.write(0);
+
+  }
+  else if (panel_3 <= thr && var_3 == 2){
+    var_3 = 0;
+  }
+
+  /*
+      //Chekeo del panel 4
+          //chanel 4 = 147
+          if (panel_4 > 300 && var_4 == 0) {
+            var_4 = 1;
+           // Serial.println(panel1);
+            Serial.write(147);
+            Serial.write(60);
+            Serial.write(100);
+            //Serial.println(panel1);
+
           }
+          else if (panel_4 < 300 && var_4 == 1){
+            var_4 = 2;
+            Serial.write(147);
+            Serial.write(60);
+            Serial.write(0);
 
-
-    //Chekeo del panel 4
-        //chanel 4 = 147
-        if (panel_4 > 300 && var_4 == 0) {
-          var_4 = 1;
-         // Serial.println(panel1);
-          Serial.write(147);
-          Serial.write(60);
-          Serial.write(100);
-          //Serial.println(panel1);
-          
-        }
-        else if (panel_4 < 300 && var_4 == 1){
-          var_4 = 2;
-          Serial.write(147);
-          Serial.write(60);
-          Serial.write(0);
-          
-        }
-        else{
-          var_4=0;
           }
-          
+          else{
+            var_4=0;
+            }
+  */
 }
